02b3d08670704acb487bae680780741b97f77af7
jtb8vm <jtb8vm@virginia.edu>
2017-12-14 20:16:38 -0500

    create URLs and shorten URLs

--- s_mservices/mservices/urls.py
+++ s_mservices/mservices/urls.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from . import views
 
 urlpatterns = [
-    url(r'^get_url$', views.get_url, name='get_url'),
+    url(r'^get_urls$', views.get_urls, name='get_urls'),
     url(r'^post_url$', views.post_url, name='post_url'),
+    url(r'^([0-9a-zA-Z]+)', views.redirect, name='redirect'),
 ]
--- s_mservices/mservices/views.py
+++ s_mservices/mservices/views.py
@@ -1,32 +1,60 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.forms import model_to_dict
 from mservices.models import Url
 from mservices.forms import CreateShortenedURL
+import string
 
-# Create your views here.
 def success_response(data):
 	return JsonResponse({"success":True, "data":data})
 
 def error_response(message):
 	return JsonResponse({"success":False, "error":message})
 
+# helper function to map IDs to short alphanumeric names
+def shortname(id):
+	digs = [] # digits
+	alphabet = string.digits + string.ascii_letters
+	# convert the id to the base of the alphabet, 62 for ([0-9a-zA-Z])
+	while id > 0:
+		digs.append(alphabet[id % 62])
+		id = id // 62
+	digs.reverse()
+	short_id = "".join(str(dig) for dig in digs)
+	return short_id
+
 @csrf_exempt
 def post_url(request):
 	if request.method == "POST":
+		# django form to help with input validation
 		create_url = CreateShortenedURL(request.POST)
 		if create_url.is_valid():
-			# TODO: generate the shortened URL, stick into DB
-
-			return success_response("test post success")
+			try:
+				existing_url = Url.objects.get(desktop_target=create_url.cleaned_data["desktop_target"])
+				return error_response("URL already exists")
+			except:
+				url = create_url.save() # save so we can get out the id
+				url_id = model_to_dict(url)["id"]
+				url.short_name = shortname(url_id)
+				url.save()
+				return success_response(model_to_dict(url))
 		else:
-			return error_response("formatting error")
+			return error_response(create_url.errors)
 	else:
-		return error_response("did not recieve post")
+		return error_response("invalid HTTP method type")
 
 @csrf_exempt
-def get_url(request):
+def redirect(request, shortname):
 	if request.method == "GET":
-		return success_response("test get success")
+		return success_response(shortname)
 	else:
-		return error_response("did not recieve get")
+		return error_response("invalid HTTP method type")
+
+@csrf_exempt
+def get_urls(request):
+	if request.method == "GET":
+		urls = Url.objects.all().values()
+		return success_response([url for url in urls])
+	else:
+		return error_response("invalid HTTP method type")
