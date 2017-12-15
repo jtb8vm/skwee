481dd5503e4276b968c1c842bce5e6f7459c0392
jtb8vm <jtb8vm@virginia.edu>
2017-12-15 08:52:53 -0500

    added full redirect support

--- s_mservices/mservices/models.py
+++ s_mservices/mservices/models.py
@@ -7,10 +7,13 @@ class Url(models.Model):
 	short_name = models.CharField(max_length=100, default="")
 	# timestamp
 	time_created = models.DateTimeField(auto_now_add=True)
-	# keep track of redirects
-	redirect_count = models.IntegerField(default=0)
 
-	# target urls for different platforms
+	# target urls for different platforms with soecific redirect counts for each
 	mobile_target = models.CharField(max_length=300, default="")
+	mobile_redirects = models.IntegerField(default=0)
+
 	desktop_target = models.CharField(max_length=300, default="")
+	desktop_redirects = models.IntegerField(default=0)
+
 	tablet_target = models.CharField(max_length=300, default="")
+	tablet_redirects = models.IntegerField(default=0)
--- s_mservices/mservices/views.py
+++ s_mservices/mservices/views.py
@@ -16,13 +16,14 @@ def error_response(message):
 def shortname(id):
 	digs = [] # digits
 	alphabet = string.digits + string.ascii_letters
+	id += 4000 # remove low-digits
 	# convert the id to the base of the alphabet, 62 for ([0-9a-zA-Z])
 	while id > 0:
 		digs.append(alphabet[id % 62])
 		id = id // 62
 	digs.reverse()
-	short_id = "".join(str(dig) for dig in digs)
-	return short_id
+	short_name = "".join(str(dig) for dig in digs)
+	return short_name
 
 @csrf_exempt
 def post_url(request):
@@ -30,15 +31,11 @@ def post_url(request):
 		# django form to help with input validation
 		create_url = CreateShortenedURL(request.POST)
 		if create_url.is_valid():
-			try:
-				existing_url = Url.objects.get(desktop_target=create_url.cleaned_data["desktop_target"])
-				return error_response("URL already exists")
-			except:
-				url = create_url.save() # save so we can get out the id
-				url_id = model_to_dict(url)["id"]
-				url.short_name = shortname(url_id)
-				url.save()
-				return success_response(model_to_dict(url))
+			url = create_url.save() # save so we can get out the id
+			url_id = model_to_dict(url)["id"]
+			url.short_name = shortname(url_id)
+			url.save()
+			return success_response(model_to_dict(url))
 		else:
 			return error_response(create_url.errors)
 	else:
@@ -54,12 +51,18 @@ def redirect(request, shortname):
 
 			# redirect to the url based on platform (using django user agents)
 			if (request.user_agent.is_mobile):
+				url.mobile_redirects += 1  # increment the redirect count
+				url.save()
 				return HttpResponseRedirect(url_dict["mobile_target"])
 
 			if (request.user_agent.is_tablet):
+				url.tablet_redirects += 1
+				url.save()
 				return HttpResponseRedirect(url_dict["tablet_target"])
 
 			if(request.user_agent.is_pc):
+				url.desktop_redirects += 1
+				url.save()
 				return HttpResponseRedirect(url_dict["desktop_target"])
 
 			return error_response("Unsupported platform.")
